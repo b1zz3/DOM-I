a76fa9acb0e896c26ea203b4019b1aade4d55875
b1zz3 <donhager23@gmail.com>
2019-10-31 11:07:41 -0400

    Finished image src update

--- js/index.js
+++ js/index.js
@@ -39,10 +39,10 @@ const siteContent = {
 
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
-let cta = document.getElementById("cat-img");
+let cta = document.getElementById("cta-img");
 cta.setAttribute('src', siteContent["cta"]["img-src"]);
 
 let midImg = document.getElementById("middle-img");
-midImg.setAttribute('src', siteContent[])
+midImg.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
